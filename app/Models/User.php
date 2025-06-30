fdd3b80e6b5896226196997ea124b8c4a0812e1a
Richard Manansala <richard232manansala@gmail.com>
2025-06-30 10:52:24 +0800

    Setup user model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    protected $table = 'booking_users';
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
