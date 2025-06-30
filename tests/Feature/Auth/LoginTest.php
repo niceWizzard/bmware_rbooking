b4dd632151b92dc80e71230c60e06c5e12687478
Richard Manansala <richard232manansala@gmail.com>
2025-06-30 15:20:05 +0800

    Fixed LoginTest

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -33,9 +33,7 @@ test('users can not authenticate with invalid password', function () {
 
 test('users can logout', function () {
     $user = User::factory()->create();
-
-    $response = $this->actingAs($user)->post('/logout');
-
+    $response = $this->actingAs($user)->post(route('logout'));
     $this->assertGuest();
     $response->assertRedirect('/');
 });
