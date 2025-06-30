4cbdd3b111851c0170abf718bd6bc1287e0d70ef
Richard Manansala <richard232manansala@gmail.com>
2025-06-30 15:14:47 +0800

    Added Model UserTest

---
+++ tests/Feature/Models/UserTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\User;
+
+test('with patient does not auto create a patient', function () {
+    $user = User::factory()->withPatient()->make();
+
+    $this->assertDatabaseCount('booking_users', 0);
+    $this->assertDatabaseCount('patients', 0);
+    $this->assertNotNull($user->patient);
+});
+
+test('user can be created', function () {
+    $user = User::factory()->withPatient()->create();
+    $this->assertNotNull($user->patient);
+});
+
+
+test('user has is<role> attributes', function () {
+    $user = User::factory()->withPatient()->create();
+    $this->assertTrue($user->is_patient);
+    $this->assertFalse($user->is_admin);
+});
