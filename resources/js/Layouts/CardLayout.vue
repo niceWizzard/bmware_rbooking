79be325ead076adc7b444eece5589cf5b0184c85
Richard Manansala <richard232manansala@gmail.com>
2025-06-30 11:53:52 +0800

    Created CardLayout

---
+++ resources/js/Layouts/CardLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+    header?: string
+}>()
+</script>
+
+<template>
+    <main class="w-full min-h-screen grid place-items-center">
+        <section class="rounded-2xl border bg-white shadow-sm p-6">
+            <!-- Check for named slot 'header' first, then fallback to prop -->
+            <div v-if="$slots.header || header" class="mb-4 text-xl font-semibold">
+                <slot name="header">
+                    {{ header }}
+                </slot>
+            </div>
+
+            <!-- Default slot -->
+            <div>
+                <slot />
+            </div>
+        </section>
+    </main>
+</template>
