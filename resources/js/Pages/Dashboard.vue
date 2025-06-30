08f13d0965883ef9ce7013a19dc29462f7089114
Richard Manansala <richard232manansala@gmail.com>
2025-06-30 16:30:12 +0800

    Made auth.user be nullable

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+  import Button from 'primevue/button'
+  import {router, usePage} from '@inertiajs/vue3'
+
+  const {props} = usePage();
+  const {user} = props.auth;
 
 </script>
 
 <template>
     Dashboard
+  <div v-if="user">
+      <p >
+        {{user}}
+
+      </p>
+      <Button @click="router.post(route('logout'))">
+        Logout
+      </Button>
+  </div>
+  <p v-else>
+    NO USER
+  </p>
+
 </template>
 
 
