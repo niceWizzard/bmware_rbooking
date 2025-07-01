360d40e4daf497bbe3292bd217eea69073226633
Richard Manansala <richard232manansala@gmail.com>
2025-07-01 13:39:16 +0800

    Added tests for the time attributes

--- tests/Feature/Models/DoctorScheduleTest.php
+++ tests/Feature/Models/DoctorScheduleTest.php
@@ -3,6 +3,7 @@
 use App\Enums\DayOfWeek;
 use App\Models\Doctor;
 use App\Models\DoctorSchedule;
+use Illuminate\Support\Carbon;
 
 test('doctor should have schedule',  function () {
     $doctor = Doctor::factory()->create();
@@ -20,4 +21,33 @@ test('doctor should have schedule',  function () {
         ->not()
         ->toBeNull();
 });
+
+test('expect start,end to be date', function () {
+    $schedule = DoctorSchedule::factory()->createDoctor()->create([
+        'start' => now()->setHour(10)->toTimeString(),
+        'end' => now()->setHour(20)->toTimeString(),
+    ]);
+    expect(
+        $schedule->start
+    )
+        ->toBeInstanceOf(Carbon::class)
+        ->and($schedule->start->hour)->toEqual(10)
+    ->and($schedule->end)
+        ->toBeInstanceOf(Carbon::class)
+        ->and( $schedule->end->hour)->toEqual(20);
+});
+
+
+test('expect break_start,break_end to be date', function () {
+    $schedule = DoctorSchedule::factory()->createDoctor()->create([
+        'break_start' => now()->setHour(10)->toTimeString(),
+        'break_end' => now()->setHour(11)->toTimeString(),
+    ]);
+    expect(
+        $schedule->break_start
+    )->toBeInstanceOf(Carbon::class)
+        ->and($schedule->break_end)
+        ->toBeInstanceOf(Carbon::class);
+});
+
 
