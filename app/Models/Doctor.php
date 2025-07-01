0b62428a0cd5f89984d4526d758fa19f036baa71
Richard Manansala <richard232manansala@gmail.com>
2025-07-01 11:08:39 +0800

    Added relationships and fillable

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Doctor extends Model
 {
     protected $table = 'booking_doctors';
     /** @use HasFactory<\Database\Factories\DoctorFactory> */
     use HasFactory;
+
+    public function schedules() : HasMany {
+        return $this->hasMany(DoctorSchedule::class, 'doctor_id');
+    }
+
+
 }
--- app/Models/DoctorSchedule.php
+++ app/Models/DoctorSchedule.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Enums\DayOfWeek;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DoctorSchedule extends Model
 {
@@ -12,7 +13,26 @@ class DoctorSchedule extends Model
     /** @use HasFactory<\Database\Factories\DoctorScheduleFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'doctor_id',
+        'start',
+        'end',
+        'day',
+        'break_start',
+        'break_end',
+    ];
+
     protected $casts = [
         'day' => DayOfWeek::class,
+        'start' => 'datetime:H:i:s',
+        'end' => 'datetime:H:i:s',
+        'break_start' => 'datetime:H:i:s',
+        'break_end' => 'datetime:H:i:s',
     ];
+
+    public function doctor(): BelongsTo {
+        return $this->belongsTo(Doctor::class);
+    }
+
+
 }
