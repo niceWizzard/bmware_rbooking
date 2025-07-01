1cbea272dff8a4248e4cdc76d5c76a35a8d485ee
Richard Manansala <richard232manansala@gmail.com>
2025-07-01 11:38:38 +0800

    Created a seed patient user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
+        User::factory()->withPatient([
+            'first_name' => 'Firsty',
+            'last_name' => 'Russell',
+        ])->create([
+            'email' => '1@email.com',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
