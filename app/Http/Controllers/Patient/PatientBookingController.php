5ae6773dda68bd4aa76f8b5a69a38dac14595cd4
Richard Manansala <richard232manansala@gmail.com>
2025-07-02 15:55:41 +0800

    Implemented the storing of doctor code in cookie

--- app/Http/Controllers/Patient/PatientBookingController.php
+++ app/Http/Controllers/Patient/PatientBookingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Patient;
 use App\Http\Controllers\Controller;
 use App\Models\Doctor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -22,10 +23,30 @@ class PatientBookingController extends Controller
     public function showBooking(Request $request): Response
     {
         $code = $request->query('code');
-        $doctor = Doctor::whereCode($code)->firstOrFail();
+        \Auth::user()->load('patient');
+
+        if(is_null($code)) {
+            $code = Cookie::get('doctorCode');
+        }
+        if(is_null($code)) {
+            return Inertia::render('Patient/Schedule/Booking', [
+                'invalid' => true,
+            ]);
+        }
+        $doctor = Doctor::whereCode($code)->first();
+
+        if(is_null($doctor)) {
+            return Inertia::render('Patient/Schedule/Booking', [
+                'invalid' => true,
+            ]);
+        }
+        Cookie::queue(Cookie::make('doctorCode', $doctor->code, 24 * 60 * 7));
 
         return Inertia::render('Patient/Schedule/Booking', [
+            'invalid' => false,
             'doctor' => $doctor,
         ]);
+
+
     }
 }
--- resources/js/Pages/Patient/Schedule/Booking.vue
+++ resources/js/Pages/Patient/Schedule/Booking.vue
@@ -1,13 +1,57 @@
 <script setup lang="ts">
     import {Doctor} from "@/types";
+    import AuthLayout from "@/Layouts/AuthLayout.vue";
+    import FullCalendar from "@fullcalendar/vue3";
+    import {CalendarOptions} from "@fullcalendar/core";
+    import timeGridPlugin from "@fullcalendar/timegrid";
+    import {computed} from "vue";
+    import {Link} from "@inertiajs/vue3";
 
-    defineProps<{
-        doctor: Doctor;
-    }>();
+    type BookingProps =
+        | { invalid: true }
+        | { invalid: false, doctor: Doctor }
+
+    const props = withDefaults(defineProps<{
+        invalid: boolean,
+        doctor?: Doctor
+    }>(), {
+        invalid: false
+    });
+
+
+    const calendarOptions: CalendarOptions = {
+        plugins: [timeGridPlugin],
+        initialView: 'timeGridWeek',
+        allDaySlot: false,
+        nowIndicator: false,
+        stickyFooterScrollbar: true,
+        slotDuration: '00:60:00',
+        expandRows: true,
+        dayHeaderFormat: { weekday: 'long' },
+        timeZone: "utc",
+        height: '100vh',
+        headerToolbar: false,
+    }
+
 </script>
 
 <template>
-    Booking with {{doctor.name}}
+    <AuthLayout header-title="Booking" >
+        <section class="p-8 flex flex-col" v-if="!invalid">
+            <h3 class="text-lg font-medium"> Dr. {{doctor!.name}}</h3>
+            <div class="flex-1 p-4">
+                <FullCalendar  :options="calendarOptions" />
+            </div>
+        </section>
+        <section class="w-full flex flex-col gap-4 p-8 items-center" v-else>
+            <p class="text-center text-2xl">
+                Invalid Doctor
+            </p>
+            <Link :href="route('patient.doctors')"
+                class="px-4 py-2 text-white bg-green-600 rounded-md"
+            >See Doctor's List</Link>
+        </section>
+    </AuthLayout>
 </template>
 
 <style scoped>
