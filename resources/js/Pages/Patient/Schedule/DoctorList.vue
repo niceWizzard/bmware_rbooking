b379c949f20e00541000a7978d3ea9b0e5c2d17f
Richard Manansala <richard232manansala@gmail.com>
2025-07-07 11:25:01 +0800

    Added logout button in doctors list

--- resources/js/Pages/Patient/Schedule/DoctorList.vue
+++ resources/js/Pages/Patient/Schedule/DoctorList.vue
@@ -1,33 +1,47 @@
 <script setup lang="ts">
 import CardLayout from '@/Layouts/CardLayout.vue';
 import { Doctor } from '@/types';
-import { Head, Link, usePage } from '@inertiajs/vue3';
+import { Head, Link, router, usePage } from '@inertiajs/vue3';
+import Button from 'primevue/button';
 import Card from 'primevue/card';
+
 defineProps<{
     doctors: Doctor[];
 }>();
 const {
     props: { auth },
 } = usePage();
+
+function logout() {
+    router.post(route('logout'));
+}
 </script>
 
 <template>
     <Head title="List of Doctors" />
     <CardLayout class="w-full">
         <template #header>
-            <div class="flex justify-end gap-2" v-if="!auth.user">
+            <div class="flex justify-end gap-2">
                 <Link
                     :href="route('login')"
                     class="rounded-md px-3 py-2 text-sm font-light"
+                    v-if="!auth.user"
                     >Login</Link
                 >
+                <Button
+                    label="Logout"
+                    variant="text"
+                    severity="danger"
+                    @click="logout"
+                />
             </div>
             <h3 class="text-center text-2xl font-bold">Doctors</h3>
             <p class="text-center text-sm font-light text-gray-600">
                 All the available doctors
             </p>
         </template>
-        <div class="flex w-full max-w-4xl flex-wrap gap-4"><Card
+        <div class="flex w-full max-w-4xl flex-wrap gap-4">
+            <Card
                 v-for="doctor in doctors"
                 class="w-72"
                 v-bind:key="'Doctor-' + doctor.id"
