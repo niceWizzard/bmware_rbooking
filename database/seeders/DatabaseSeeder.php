0970761609dd05f2e81bdc6b448c6dabb8b12037
Richard Manansala <richard232manansala@gmail.com>
2025-07-07 13:39:50 +0800

    Made the seeder only generate user on non production env

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,31 +2,20 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        User::factory()->withPatient([
-            'first_name' => 'Firsty',
-            'last_name' => 'Russell',
-        ])->create([
-            'email' => '1@email.com',
-            'password' => Hash::make('password'),
-        ]);
-
-        User::factory()->withAdmin([
-            'name' => "ADMIN USER",
-        ])->create([
-            'email' => 'admin@email.com',
-            'password' => Hash::make('password'),
-        ]);
+        $this->call(AdminSeeder::class);
+        if (!App::environment('production')) {
+            $this->call(UserSeeder::class);
+        }
     }
 }
