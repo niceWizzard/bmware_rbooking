34f1fb42d435d7470b9f9183ed2ac04dfc5ccaa9
Richard Manansala <richard232manansala@gmail.com>
2025-07-09 08:52:07 +0800

    Improved the structure of middleware group function

--- routes/admin.php
+++ routes/admin.php
@@ -8,36 +8,37 @@ use App\Models\Doctor;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
-Route::prefix('/admin')
-    ->name('admin')
-    ->middleware(['auth', 'only.admin'])
-    ->group(static function () {
-        Route::get('/dashboard', [DashboardController::class,'index'])->name('.dashboard');
-        Route::get('/dashboard/fetch', [DashboardController::class,'fetchAppointmentSlots'])->name('.dashboard.fetch');
-        Route::post('/', [AdminInfoController::class, 'update'])->name('.profile');
+Route::middleware(['auth', 'only.admin'])
+->group(static function () {
+
+    Route::prefix('/admin')
+        ->name('admin')
+        ->group(static function () {
+            Route::get('/dashboard', [DashboardController::class,'index'])->name('.dashboard');
+            Route::get('/dashboard/fetch', [DashboardController::class,'fetchAppointmentSlots'])->name('.dashboard.fetch');
+            Route::post('/', [AdminInfoController::class, 'update'])->name('.profile');
     });
 
-Route::middleware(['auth', 'only.admin'])
-    ->prefix('/admin/doctors')
-    ->name('doctor')
-    ->controller(DoctorController::class)
+
+    Route::prefix('/admin/doctors')
+        ->name('doctor')
+        ->controller(DoctorController::class)
+        ->group(static function () {
+
+        Route::post('/create', 'store');
+        Route::get('/create', 'create')->name('.create');
+        Route::get('/', 'index')->name('.list');
+        Route::delete('/{id}', 'delete')->name('.delete');
+    });
+
+    Route::prefix('/schedule/{id}')
+    ->name('schedule')
+    ->controller(DoctorScheduleController::class)
     ->group(static function () {
-
-    Route::post('/create', 'store');
-    Route::get('/create', 'create')->name('.create');
-    Route::get('/', 'index')->name('.list');
-    Route::delete('/{id}', 'delete')->name('.delete');
-});
-
-Route::middleware(['auth','only.admin'])
-->prefix('/schedule/{id}')
-->name('schedule')
-->controller(DoctorScheduleController::class)
-->group(static function () {
-    Route::get('/create', 'create')->name('.create');
-    Route::post('/create', 'store');
-    Route::get('/', 'view')->name('.view');
-
-    Route::get('/edit', 'edit')->name('.edit');
-    Route::post('/edit', 'update');
+        Route::get('/create', 'create')->name('.create');
+        Route::post('/create', 'store');
+        Route::get('/', 'view')->name('.view');
+        Route::get('/edit', 'edit')->name('.edit');
+        Route::post('/edit', 'update');
+    });
 });
